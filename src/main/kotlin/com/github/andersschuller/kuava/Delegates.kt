9f079eb3a8d1f4e22a8fd9626d824c72b4064224
Anders Schuller <andersschuller@gmail.com>
2015-09-16 20:29:25 +0100

    Upgrade to Kotlin 0.13.1513

--- src/main/kotlin/com/github/andersschuller/kuava/Delegates.kt
+++ src/main/kotlin/com/github/andersschuller/kuava/Delegates.kt
@@ -6,17 +6,17 @@ import kotlin.properties.ReadWriteProperty
 
 object Delegates {
     fun cacheVal<T>(cache: Cache<String, T>) = object : ReadOnlyProperty<Any?, T> {
-        override fun get(thisRef: Any?, desc: PropertyMetadata): T {
-            return cache.getIfPresent(desc.name)
+        override fun get(thisRef: Any?, property: PropertyMetadata): T {
+            return cache.getIfPresent(property.name)
         }
     }
 
     fun cacheVar<T>(cache: Cache<String, T>) = object : ReadWriteProperty<Any?, T> {
-        override fun get(thisRef: Any?, desc: PropertyMetadata): T {
-            return cache.getIfPresent(desc.name)
+        override fun get(thisRef: Any?, property: PropertyMetadata): T {
+            return cache.getIfPresent(property.name)
         }
-        override fun set(thisRef: Any?, desc: PropertyMetadata, value: T) {
-            cache.put(desc.name, value)
+        override fun set(thisRef: Any?, property: PropertyMetadata, value: T) {
+            cache.put(property.name, value)
         }
     }
 }
