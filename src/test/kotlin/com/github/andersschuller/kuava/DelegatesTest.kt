ab6519100b6f36f2584bfa33acd1a33dae0e69b1
Anders Schuller <andersschuller@gmail.com>
2015-09-16 20:26:07 +0100

    Convert specs to standard JUnit tests

---
+++ src/test/kotlin/com/github/andersschuller/kuava/DelegatesTest.kt
@@ -0,0 +1,36 @@
+package com.github.andersschuller.kuava
+
+import com.google.common.cache.*
+import kotlin.test.*
+import org.junit.Test
+
+class DelegatesTest {
+    val cache = newCache()
+
+    @Test fun delegateValToCache() {
+        val example = CacheValExample(cache)
+        assertEquals(cache.getIfPresent("foo"), example.foo)
+    }
+
+    @Test fun delegateVarToCache() {
+        val example = CacheVarExample(cache)
+        example.bar = 43
+        assertEquals(example.bar, 43)
+        assertEquals(cache.getIfPresent("bar"), 43)
+    }
+
+    fun newCache(): Cache<String, Int> {
+        val cache: Cache<String, Int> = CacheBuilder.newBuilder().build()
+        cache.put("foo", 1)
+        cache.put("bar", 2)
+        return cache
+    }
+
+    class CacheValExample(cache: Cache<String, Int>) {
+        val foo: Int by Delegates.cacheVal(cache)
+    }
+
+    class CacheVarExample(cache: Cache<String, Int>) {
+        var bar: Int by Delegates.cacheVar(cache)
+    }
+}
---
+++ src/test/kotlin/com/github/andersschuller/kuava/FunctionsTest.kt
@@ -0,0 +1,48 @@
+package com.github.andersschuller.kuava
+
+import com.google.common.base.Function as GuavaFunction
+import com.google.common.base.Functions as GuavaFunctions
+import com.google.common.base.Predicate as GuavaPredicate
+import com.google.common.base.Predicates as GuavaPredicates
+import com.google.common.base.Supplier as GuavaSupplier
+import com.google.common.base.Suppliers as GuavaSuppliers
+import kotlin.test.*
+import org.junit.Test
+
+class FunctionsTest {
+    @Test fun convertFunctionToGuavaFunction() {
+        val f: (String) -> Int = { s -> s.length() }
+        val g: GuavaFunction<String, Int> = f.toGuavaFunction()
+        assertEquals(f("foo"), g.apply("foo"))
+    }
+
+    @Test fun convertPredicateFunctionToGuavaPredicate() {
+        val f: (Int) -> Boolean = { i -> i % 2 == 0 }
+        val g: GuavaPredicate<Int> = f.toGuavaPredicate()
+        assertEquals(f(42), g.apply(42))
+    }
+
+    @Test fun convertSupplierFunctionToGuavaSupplier() {
+        val f: () -> Long = { -> 1024 }
+        val g: GuavaSupplier<Long> = f.toGuavaSupplier()
+        assertEquals(f(), g.get())
+    }
+
+    @Test fun convertGuavaFunctionToFunction() {
+        val g: GuavaFunction<Any, String> = GuavaFunctions.toStringFunction()
+        val f: (Any) -> String = g.toFunction()
+        assertEquals(g.apply(12), f(12))
+    }
+
+    @Test fun convertGuavaPredicateToFunction() {
+        val g: GuavaPredicate<String> = GuavaPredicates.equalTo("foo")
+        val f: (String) -> Boolean = g.toFunction()
+        assertEquals(g.apply("bar"), f("bar"))
+    }
+
+    @Test fun convertGuavaSupplierToFunction() {
+        val g: GuavaSupplier<Char> = GuavaSuppliers.ofInstance('Y')
+        val f: () -> Char = g.toFunction()
+        assertEquals(g.get(), f())
+    }
+}
