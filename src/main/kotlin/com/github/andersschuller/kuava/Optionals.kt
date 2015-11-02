65b7e355ec54206c4b1a3566bab9093aa1caddf7
Anders Schuller <andersschuller@gmail.com>
2015-11-02 18:33:54 +0000

    Remove unnecessary suppression of compiler warning

--- src/main/kotlin/com/github/andersschuller/kuava/Optionals.kt
+++ src/main/kotlin/com/github/andersschuller/kuava/Optionals.kt
@@ -2,5 +2,4 @@ package com.github.andersschuller.kuava
 
 import com.google.common.base.Optional
 
-@Suppress("BASE_WITH_NULLABLE_UPPER_BOUND")
 fun <T> T?.toOptional(): Optional<T> = Optional.fromNullable(this)
