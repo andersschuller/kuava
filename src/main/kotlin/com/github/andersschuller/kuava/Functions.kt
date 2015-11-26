87f7df71baaef8f9895ba05a61bf8f72456146ba
Anders Schuller <andersschuller@gmail.com>
2015-11-26 19:53:38 +0000

    Use lambdas in Functions.kt

--- src/main/kotlin/com/github/andersschuller/kuava/Functions.kt
+++ src/main/kotlin/com/github/andersschuller/kuava/Functions.kt
@@ -4,17 +4,11 @@ import com.google.common.base.Function as GuavaFunction
 import com.google.common.base.Predicate as GuavaPredicate
 import com.google.common.base.Supplier as GuavaSupplier
 
-fun <F, T> Function1<F, T>.toGuavaFunction(): GuavaFunction<F, T> = object : GuavaFunction<F, T> {
-    override fun apply(input: F): T = invoke(input)
-}
+fun <F, T> Function1<F, T>.toGuavaFunction(): GuavaFunction<F, T> = GuavaFunction { invoke(it) }
 
-fun <T> Function1<T, Boolean>.toGuavaPredicate(): GuavaPredicate<T> = object : GuavaPredicate<T> {
-    override fun apply(input: T): Boolean = invoke(input)
-}
+fun <T> Function1<T, Boolean>.toGuavaPredicate(): GuavaPredicate<T> = GuavaPredicate { invoke(it) }
 
-fun <T> Function0<T>.toGuavaSupplier(): GuavaSupplier<T> = object : GuavaSupplier<T> {
-    override fun get(): T = invoke()
-}
+fun <T> Function0<T>.toGuavaSupplier(): GuavaSupplier<T> = GuavaSupplier { invoke() }
 
 fun <F, T> GuavaFunction<F, T>.toFunction(): Function1<F, T> = { apply(it) }
 
